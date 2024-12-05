f11b3c8f836943604234fa933006a812c6456cf4
Midival <lukehays32@gmail.com>
2024-12-05 16:54:07 -0500

    day 5 partial

--- 5/solution.py
+++ 5/solution.py
@@ -1,4 +1,4 @@
-import re
+from itertools import permutations
 
 sampleTextPath = './sample.txt'
 puzzleTextPath = './puzzle.txt'
@@ -9,14 +9,55 @@ def readPuzzleInput(textPath):
     input = [line.strip() for line in file]
   return input
 
-def printGrid(grid):
-  for row in grid:
-    print(row)
+def verifyUpdateCorrectness(update, vertices):
+  for j in range(len(update)-1):
+    x, y = update[j], update[j+1]
+    if y not in vertices[x]: return False
+  return True
+
+# def fixOrder(update, i, vertices):
+#   if len(update) <= 1 : 
+#     return True
+  
+#   left = fixOrder(update[:i], i-1) and verifyUpdateCorrectness(update[:i+1], vertices)
+#   right = fixOrder(update[i+1:], i+1) and verifyUpdateCorrectness(update[:i+1], vertices)
 
 def solve(input):
   result1 = 0
   result2 = 0
 
+  vertices = {}
+  secondInstructionIndex = 0
+
+  for i in range(len(input)):
+    if input[i] == '':
+      secondInstructionIndex = i+1
+      break
+    
+    v, e = [int(x) for x in input[i].split('|')]
+
+    if v not in vertices:
+      vertices[v] = []
+    if e not in vertices:
+      vertices[e] = []
+
+    vertices[v].append(e)
+
+  incorrectUpdates = []
+  for i in range(secondInstructionIndex, len(input)):
+    update = [int(n) for n in input[i].split(',')]
+    if verifyUpdateCorrectness(update, vertices):
+      result1 += update[len(update)//2]
+    else:
+      incorrectUpdates.append(update)
+
+  for update in incorrectUpdates:
+    perms = permutations(update)
+    for perm in perms:
+      print(perm)
+      if verifyUpdateCorrectness(perm, vertices):
+        result2 += perm[len(perm)//2]
+        break
 
   print("Result 1: ", result1)
   print("Result 2: ", result2)
@@ -28,6 +69,6 @@ print("------------------")
 print("Sample")
 solve(sampleInput)
 print("------------------")
-# print("Puzzle")
-# solve(puzzleInput)
-# print("------------------")
+print("Puzzle")
+solve(puzzleInput)
+print("------------------")
