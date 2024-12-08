a871aff81d3070a3d79f7adb292e21b650d46c68
Midival <lukehays32@gmail.com>
2024-12-08 10:29:02 -0500

    add day 8 solution

--- 8/solution.py
+++ 8/solution.py
@@ -8,11 +8,54 @@ def readPuzzleInput(textPath):
   return input
 
 def solve(input):
-  result1 = 0
-  result2 = 0
+  grid = [[c for c in line] for line in input]
+  antennas = {}
+  antinodes = set()
 
-  print("Result 1: ", result1)
-  print("Result 2: ", result2)
+  for row in range(len(grid)):
+    for col in range(len(grid[row])):
+      char = grid[row][col]
+      if char != '.' and char != '#':
+        if char not in antennas:
+          antennas[char] = [(row, col)]
+        else:
+          antennas[char].append((row, col))
+
+  for k, v in antennas.items():
+    for antenna in v:
+      x1, y1 = antenna
+
+      for neighbor in v:
+        if neighbor == antenna: continue
+
+        x2, y2 = neighbor
+        rise, run = x1-x2, y1-y2
+        x3, y3 = x1+rise, y1+run
+        
+        if x3 >= 0 and x3 < len(grid) and y3 >= 0 and y3 < len(grid[0]): antinodes.add((x3, y3))
+
+  print("Result 1: ", len(antinodes))
+
+  antinodes = set()
+  for k, v in antennas.items():
+    for antenna in v:
+      x1, y1 = antenna
+
+      for neighbor in v:
+        if neighbor == antenna: continue
+
+        antinodes.add(neighbor)
+        antinodes.add(antenna)
+
+        x2, y2 = neighbor
+        rise, run = x1-x2, y1-y2
+        x3, y3 = x1+rise, y1+run
+        
+        while x3 >= 0 and x3 < len(grid) and y3 >= 0 and y3 < len(grid[0]): 
+          antinodes.add((x3, y3))
+          x3, y3 = x3+rise, y3+run
+
+  print("Result 2: ", len(antinodes))
   
 sampleInput = readPuzzleInput(sampleTextPath)
 puzzleInput = readPuzzleInput(puzzleTextPath)
